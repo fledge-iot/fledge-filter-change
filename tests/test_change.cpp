feda6df76d1eac3578995f1de91b658cb41da34c
nandan <nandan.ghildiyal@gmail.com>
2023-08-29 12:38:59 +0530

    FOGL-7911: Added test case for different value of Pre and Post trigger
    Signed-off-by: nandan <nandan.ghildiyal@gmail.com>

--- tests/test_change.cpp
+++ tests/test_change.cpp
@@ -277,6 +277,99 @@ TEST(CHANGE, PrePostTriggerData)
 	delete readingSet4;
 }
 
+TEST(CHANGE, PrePostTriggerData2)
+{
+	// Test case : pre and post trigger readings count for different preTrigger and postTrigger values
+
+	PLUGIN_INFORMATION *info = plugin_info();
+	ConfigCategory *config = new ConfigCategory("change", info->config);
+	ASSERT_NE(config, (ConfigCategory *)NULL);
+	config->setItemsValueFromDefault();
+	config->setValue("asset", "test");
+	config->setValue("trigger", "test");
+	config->setValue("change", "50");
+	config->setValue("preTrigger", "2000");
+	config->setValue("postTrigger", "1000");
+	config->setValue("rate", "0");
+	config->setValue("rateUnit", "per second");
+	config->setValue("enable", "true");
+
+	ReadingSet *outReadings;
+	void *handle = plugin_init(config, &outReadings, Handler);
+
+	// First Set of readings
+	vector<Reading *> *readings1 = new vector<Reading *>;
+	long testValue1 = 11;
+	DatapointValue dpv1(testValue1);
+	Datapoint *value1 = new Datapoint("test", dpv1);
+	Reading *in1 = new Reading("test", value1);
+	readings1->push_back(in1);
+	ReadingSet *readingSet1 = new ReadingSet(readings1);
+
+	plugin_ingest(handle, (READINGSET *)readingSet1);
+	vector<Reading *> results1 = outReadings->getAllReadings();
+	sleep(2);
+
+	// Second Set of readings
+	vector<Reading *> *readings2 = new vector<Reading *>;
+	long testValue2 = 12;
+	DatapointValue dpv2(testValue2);
+	Datapoint *value2 = new Datapoint("test", dpv2);
+	Reading *in2 = new Reading("test", value2);
+	readings2->push_back(in2);
+
+	long testValue3 = 13;
+	DatapointValue dpv3(testValue3);
+	Datapoint *value3 = new Datapoint("test", dpv3);
+	Reading *in3 = new Reading("test", value3);
+	readings2->push_back(in3);
+	ReadingSet *readingSet2 = new ReadingSet(readings2);
+
+	plugin_ingest(handle, (READINGSET *)readingSet2);
+	vector<Reading *> results2 = outReadings->getAllReadings();
+	ASSERT_EQ(results2.size(), 0); // trigger condition didn't meet
+	sleep(2);
+
+	// Third Set of readings
+	vector<Reading *> *readings3 = new vector<Reading *>;
+	long testValue4 = 25;
+	DatapointValue dpv4(testValue4);
+	Datapoint *value4 = new Datapoint("test", dpv4);
+	Reading *in4 = new Reading("test", value4);
+	readings3->push_back(in4);
+	ReadingSet *readingSet3 = new ReadingSet(readings3);
+
+	plugin_ingest(handle, (READINGSET *)readingSet3);
+	vector<Reading *> results3 = outReadings->getAllReadings();
+	ASSERT_EQ(results3.size(), 3); // trigger condition met - PreTrigger Data
+
+	// Fourth Set of readings
+	vector<Reading *> *readings4 = new vector<Reading *>;
+	long testValue5 = 26;
+	DatapointValue dpv5(testValue5);
+	Datapoint *value5 = new Datapoint("test", dpv5);
+	Reading *in5 = new Reading("test", value5);
+	readings4->push_back(in5);
+	ReadingSet *readingSet4 = new ReadingSet(readings4);
+
+	plugin_ingest(handle, (READINGSET *)readingSet4);
+	vector<Reading *> results4 = outReadings->getAllReadings();
+	ASSERT_EQ(results4.size(), 1);  // Post Trigger Data
+
+	//Memory cleanup
+	delete readings1;
+	delete readingSet1;
+
+	delete readings2;
+	delete readingSet2;
+
+	delete readings3;
+	delete readingSet3;
+
+	delete readings4;
+	delete readingSet4;
+}
+
 TEST(CHANGE, PrePostTriggerStringData)
 {
 	// Test case : String data point
