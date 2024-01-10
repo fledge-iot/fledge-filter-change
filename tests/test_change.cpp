32e24100eb57f92c879dae77b41b939ed2f2b688
nandan <nandan.ghildiyal@gmail.com>
2024-01-10 12:19:55 +0530

    FOGL-8372 : Removed memory cleanup code as filter takes care of it
    Signed-off-by: nandan <nandan.ghildiyal@gmail.com>

--- tests/test_change.cpp
+++ tests/test_change.cpp
@@ -108,32 +108,32 @@ TEST(CHANGE, triggerNotMatched)
 	void *handle = plugin_init(config, &outReadings, Handler);
 
 	// First Set of readings 
-	vector<Reading *> *readings1 = new vector<Reading *>;
+	vector<Reading *> readings1;
 	long testValue1 = 11;
 	DatapointValue dpv1(testValue1);
 	Datapoint *value1 = new Datapoint("test", dpv1);
 	Reading *in1 = new Reading("test", value1);
-	readings1->push_back(in1);
-	ReadingSet *readingSet1 = new ReadingSet(readings1);
+	readings1.push_back(in1);
+	ReadingSet *readingSet1 = new ReadingSet(&readings1);
 
 	plugin_ingest(handle, (READINGSET *)readingSet1);
 	vector<Reading *> results1 = outReadings->getAllReadings();
 	sleep(2);
 
 	// Second Set of readings 
-	vector<Reading *> *readings2 = new vector<Reading *>;
+	vector<Reading *> readings2;
 	long testValue2 = 12;
 	DatapointValue dpv2(testValue2);
 	Datapoint *value2 = new Datapoint("test", dpv2);
 	Reading *in2 = new Reading("test", value2);
-	readings2->push_back(in2);
+	readings2.push_back(in2);
 
 	long testValue3 = 13;
 	DatapointValue dpv3(testValue3);
 	Datapoint *value3 = new Datapoint("test", dpv3);
 	Reading *in3 = new Reading("test", value3);
-	readings2->push_back(in3);
-	ReadingSet *readingSet2 = new ReadingSet(readings2);
+	readings2.push_back(in3);
+	ReadingSet *readingSet2 = new ReadingSet(&readings2);
 
 	plugin_ingest(handle, (READINGSET *)readingSet2);
 	vector<Reading *> results2 = outReadings->getAllReadings();
@@ -141,13 +141,13 @@ TEST(CHANGE, triggerNotMatched)
 	ASSERT_EQ(results2.size(), 0); // trigger condition didn't meet
 
 	// Third Set of readings 
-	vector<Reading *> *readings3 = new vector<Reading *>;
+	vector<Reading *> readings3;
 	long testValue4 = 14;
 	DatapointValue dpv4(testValue4);
 	Datapoint *value4 = new Datapoint("test", dpv4);
 	Reading *in4 = new Reading("test", value4);
-	readings3->push_back(in4);
-	ReadingSet *readingSet3 = new ReadingSet(readings3);
+	readings3.push_back(in4);
+	ReadingSet *readingSet3 = new ReadingSet(&readings3);
 
 	plugin_ingest(handle, (READINGSET *)readingSet3);
 	vector<Reading *> results3 = outReadings->getAllReadings();
@@ -156,30 +156,17 @@ TEST(CHANGE, triggerNotMatched)
 	sleep(1);
 	
 	// Fourth Set of readings 
-	vector<Reading *> *readings4 = new vector<Reading *>;
+	vector<Reading *> readings4;
 	long testValue5 = 15;
 	DatapointValue dpv5(testValue5);
 	Datapoint *value5 = new Datapoint("test", dpv5);
 	Reading *in5 = new Reading("test", value5);
-	readings4->push_back(in5);
-	ReadingSet *readingSet4 = new ReadingSet(readings4);
+	readings4.push_back(in5);
+	ReadingSet *readingSet4 = new ReadingSet(&readings4);
 
 	plugin_ingest(handle, (READINGSET *)readingSet4);
 	vector<Reading *> results4 = outReadings->getAllReadings();
 	ASSERT_EQ(results4.size(), 0);  // trigger condition didn't meet
-
-	//Memory cleanup
-	delete readings1;
-	delete readingSet1;
-
-	delete readings2;
-	delete readingSet2;
-
-	delete readings3;
-	delete readingSet3;
-
-	delete readings4;
-	delete readingSet4;
 }
 
 TEST(CHANGE, PrePostTriggerData)
@@ -203,32 +190,32 @@ TEST(CHANGE, PrePostTriggerData)
 	void *handle = plugin_init(config, &outReadings, Handler);
 
 	// First Set of readings 
-	vector<Reading *> *readings1 = new vector<Reading *>;
+	vector<Reading *> readings1;
 	long testValue1 = 11;
 	DatapointValue dpv1(testValue1);
 	Datapoint *value1 = new Datapoint("test", dpv1);
 	Reading *in1 = new Reading("test", value1);
-	readings1->push_back(in1);
-	ReadingSet *readingSet1 = new ReadingSet(readings1);
+	readings1.push_back(in1);
+	ReadingSet *readingSet1 = new ReadingSet(&readings1);
 
 	plugin_ingest(handle, (READINGSET *)readingSet1);
 	vector<Reading *> results1 = outReadings->getAllReadings();
 	sleep(2);
 
 	// Second Set of readings 
-	vector<Reading *> *readings2 = new vector<Reading *>;
+	vector<Reading *> readings2;
 	long testValue2 = 12;
 	DatapointValue dpv2(testValue2);
 	Datapoint *value2 = new Datapoint("test", dpv2);
 	Reading *in2 = new Reading("test", value2);
-	readings2->push_back(in2);
+	readings2.push_back(in2);
 
 	long testValue3 = 13;
 	DatapointValue dpv3(testValue3);
 	Datapoint *value3 = new Datapoint("test", dpv3);
 	Reading *in3 = new Reading("test", value3);
-	readings2->push_back(in3);
-	ReadingSet *readingSet2 = new ReadingSet(readings2);
+	readings2.push_back(in3);
+	ReadingSet *readingSet2 = new ReadingSet(&readings2);
 
 	plugin_ingest(handle, (READINGSET *)readingSet2);
 	vector<Reading *> results2 = outReadings->getAllReadings();
@@ -236,13 +223,13 @@ TEST(CHANGE, PrePostTriggerData)
 	ASSERT_EQ(results2.size(), 0); // trigger condition didn't meet
 
 	// Third Set of readings 
-	vector<Reading *> *readings3 = new vector<Reading *>;
+	vector<Reading *> readings3;
 	long testValue4 = 25;
 	DatapointValue dpv4(testValue4);
 	Datapoint *value4 = new Datapoint("test", dpv4);
 	Reading *in4 = new Reading("test", value4);
-	readings3->push_back(in4);
-	ReadingSet *readingSet3 = new ReadingSet(readings3);
+	readings3.push_back(in4);
+	ReadingSet *readingSet3 = new ReadingSet(&readings3);
 
 	plugin_ingest(handle, (READINGSET *)readingSet3);
 	vector<Reading *> results3 = outReadings->getAllReadings();
@@ -251,30 +238,17 @@ TEST(CHANGE, PrePostTriggerData)
 	sleep(1);
 	
 	// Fourth Set of readings 
-	vector<Reading *> *readings4 = new vector<Reading *>;
+	vector<Reading *> readings4;
 	long testValue5 = 26;
 	DatapointValue dpv5(testValue5);
 	Datapoint *value5 = new Datapoint("test", dpv5);
 	Reading *in5 = new Reading("test", value5);
-	readings4->push_back(in5);
-	ReadingSet *readingSet4 = new ReadingSet(readings4);
+	readings4.push_back(in5);
+	ReadingSet *readingSet4 = new ReadingSet(&readings4);
 
 	plugin_ingest(handle, (READINGSET *)readingSet4);
 	vector<Reading *> results4 = outReadings->getAllReadings();
 	ASSERT_EQ(results4.size(), 1);  // Post Trigger Data
-
-	//Memory cleanup
-	delete readings1;
-	delete readingSet1;
-
-	delete readings2;
-	delete readingSet2;
-
-	delete readings3;
-	delete readingSet3;
-
-	delete readings4;
-	delete readingSet4;
 }
 
 TEST(CHANGE, PrePostTriggerData2)
@@ -298,32 +272,32 @@ TEST(CHANGE, PrePostTriggerData2)
 	void *handle = plugin_init(config, &outReadings, Handler);
 
 	// First Set of readings
-	vector<Reading *> *readings1 = new vector<Reading *>;
+	vector<Reading *> readings1;
 	long testValue1 = 11;
 	DatapointValue dpv1(testValue1);
 	Datapoint *value1 = new Datapoint("test", dpv1);
 	Reading *in1 = new Reading("test", value1);
-	readings1->push_back(in1);
-	ReadingSet *readingSet1 = new ReadingSet(readings1);
+	readings1.push_back(in1);
+	ReadingSet *readingSet1 = new ReadingSet(&readings1);
 
 	plugin_ingest(handle, (READINGSET *)readingSet1);
 	vector<Reading *> results1 = outReadings->getAllReadings();
 	sleep(2);
 
 	// Second Set of readings
-	vector<Reading *> *readings2 = new vector<Reading *>;
+	vector<Reading *> readings2;
 	long testValue2 = 12;
 	DatapointValue dpv2(testValue2);
 	Datapoint *value2 = new Datapoint("test", dpv2);
 	Reading *in2 = new Reading("test", value2);
-	readings2->push_back(in2);
+	readings2.push_back(in2);
 
 	long testValue3 = 13;
 	DatapointValue dpv3(testValue3);
 	Datapoint *value3 = new Datapoint("test", dpv3);
 	Reading *in3 = new Reading("test", value3);
-	readings2->push_back(in3);
-	ReadingSet *readingSet2 = new ReadingSet(readings2);
+	readings2.push_back(in3);
+	ReadingSet *readingSet2 = new ReadingSet(&readings2);
 
 	plugin_ingest(handle, (READINGSET *)readingSet2);
 	vector<Reading *> results2 = outReadings->getAllReadings();
@@ -331,43 +305,30 @@ TEST(CHANGE, PrePostTriggerData2)
 	sleep(2);
 
 	// Third Set of readings
-	vector<Reading *> *readings3 = new vector<Reading *>;
+	vector<Reading *> readings3;
 	long testValue4 = 25;
 	DatapointValue dpv4(testValue4);
 	Datapoint *value4 = new Datapoint("test", dpv4);
 	Reading *in4 = new Reading("test", value4);
-	readings3->push_back(in4);
-	ReadingSet *readingSet3 = new ReadingSet(readings3);
+	readings3.push_back(in4);
+	ReadingSet *readingSet3 = new ReadingSet(&readings3);
 
 	plugin_ingest(handle, (READINGSET *)readingSet3);
 	vector<Reading *> results3 = outReadings->getAllReadings();
 	ASSERT_EQ(results3.size(), 3); // trigger condition met - PreTrigger Data
 
 	// Fourth Set of readings
-	vector<Reading *> *readings4 = new vector<Reading *>;
+	vector<Reading *> readings4;
 	long testValue5 = 26;
 	DatapointValue dpv5(testValue5);
 	Datapoint *value5 = new Datapoint("test", dpv5);
 	Reading *in5 = new Reading("test", value5);
-	readings4->push_back(in5);
-	ReadingSet *readingSet4 = new ReadingSet(readings4);
+	readings4.push_back(in5);
+	ReadingSet *readingSet4 = new ReadingSet(&readings4);
 
 	plugin_ingest(handle, (READINGSET *)readingSet4);
 	vector<Reading *> results4 = outReadings->getAllReadings();
 	ASSERT_EQ(results4.size(), 1);  // Post Trigger Data
-
-	//Memory cleanup
-	delete readings1;
-	delete readingSet1;
-
-	delete readings2;
-	delete readingSet2;
-
-	delete readings3;
-	delete readingSet3;
-
-	delete readings4;
-	delete readingSet4;
 }
 
 TEST(CHANGE, PrePostTriggerStringData)
@@ -390,28 +351,23 @@ TEST(CHANGE, PrePostTriggerStringData)
 	ReadingSet *outReadings;
 	void *handle = plugin_init(config, &outReadings, Handler);
 
-	vector<Reading *> *readings = new vector<Reading *>;
+	vector<Reading *> readings;
 	std::string testValue2 = "Floor1";
 	DatapointValue dpv2(testValue2);
 	Datapoint *value2 = new Datapoint("test", dpv2);
 	Reading *in2 = new Reading("test", value2);
-	readings->push_back(in2);
+	readings.push_back(in2);
 
 	std::string testValue3 = "Floor2";
 	DatapointValue dpv3(testValue3);
 	Datapoint *value3 = new Datapoint("test", dpv3);
 	Reading *in3 = new Reading("test", value3);
-	readings->push_back(in3);
-	ReadingSet *readingSet = new ReadingSet(readings);
+	readings.push_back(in3);
+	ReadingSet *readingSet = new ReadingSet(&readings);
 
 	plugin_ingest(handle, (READINGSET *)readingSet);
 	vector<Reading *> results = outReadings->getAllReadings();
 	ASSERT_EQ(results.size(), 2); // trigger condition meet
-
-	//Memory cleanup
-	delete readings;
-	delete readingSet;
-
 }
 
 TEST(CHANGE, average)
